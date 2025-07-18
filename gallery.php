38a2f57b50e8e98f0201cf999671d8279c27cb29
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2025-07-18 12:09:55 +0200

    Added Foreach loop for images

--- gallery.php
+++ gallery.php
@@ -5,5 +5,12 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<?php foreach ($imageTitles as $image => $title): ?>
+    <a href="image.php?image=<?= http_build_query(['image' => $image]); ?>">
+        <h3><?= escape($title); ?></h3>
+        <img src="<?= "./images/" . rawurlencode($image); ?>" alt="<?= rawurlencode($title); ?>" />
+    </a>
+<?php endforeach; ?>
+
 
 <?php include './views/footer.php'; ?>
