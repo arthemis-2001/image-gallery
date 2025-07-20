8bfce98f4b7cdc7acd32cddd7f7c013f8cbd0816
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2025-07-20 14:19:52 +0200

    Modified image.php a bit

--- image.php
+++ image.php
@@ -1,9 +1,6 @@
 <?php
 include './inc/functions.inc.php';
 include './inc/images.inc.php';
-
-
-
 ?>
 <?php include './views/header.php'; ?>
 
@@ -11,14 +8,18 @@ include './inc/images.inc.php';
     <?php 
     $image = $_GET['image']; 
     $title = $imageTitles[$image];
+    $description = $imageDescriptions[$image];
     ?>
     <h2><?= escape($title); ?></h2>
     <img src="<?= "./images/" . rawurlencode($image); ?>" alt="<?= escape($title); ?>" />
-    <p><?= escape($imageDescriptions[$image]); ?></p>
+    <p><?= str_replace("\n", "<br>", escape($description)); ?></p>
 <?php else: ?>
     <div class="notice">
         <p>Image not found</p>
     </div>
 <?php endif; ?>
+
+<a href="gallery.php">Back to gallery</a>
+<br /><br /><br />
 
 <?php include './views/footer.php'; ?>
